5e6f711b287784cc7628c8f7087f878447229e37
unknown <luanpaivatani@gmail.com>
2021-06-08 21:24:05 -0300

    conclusão

---
+++ nm_comp.js
@@ -0,0 +1,13 @@
+let nome= prompt("Qual seria o seu Nome?");
+
+let sobrenome= prompt("Agora diga o seu Sobrenome?");
+
+let nomecompleto= `Seu Nome Completo É: ${nome} ${sobrenome}.`
+
+alert(nomecompleto);
+
+let sobrenome2= sobrenome.toUpperCase()
+
+let nomecompleto2= `Nome de Catálogo: ${sobrenome2} ${nome}.`
+
+alert(nomecompleto2);
